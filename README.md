8a9af4660038bbf5d8b3373028b4a7e3b6816971
pm <=>
2016-04-06 03:10:52 +0200

    sob.run now is passed onNext function readme update
    *new Sob(function(onNext){} ....)
    *sob run and dispose now  set active state

--- README.md
+++ README.md
@@ -9,13 +9,40 @@ this is beginning of minimalistic library working on streams, similar to rxjs
 
 
 ##k so how do i even
-for now you can create a streams fromDomEvent(node, eventName) or fromInterval(miliseconds)
+for now you can create a streams with: fromDomEvent(node, eventName), fromInterval(miliseconds),  fromInterval(miliseconds)
+
+
+if you are crazy enough, you can also create own streams with Sob(run, dispose) constructor;
+
+```
+//!!!! not that i tested it yet
+
+otherStream.map(function(x){
+	return new Sob(
+		function(onNext){
+			var xhr = new XMLHttpRequest();
+			xhr.open('GET', 'someurl')
+			xhr.addEventListener('load', onNext, false);
+	      xhr.send();
+		},
+		function(){
+			//actually im not entirely sure yet what how woudl you cancel this
+		}
+	)
+}).flatMap(function(xhrData){return xhrData}).sub(function(data){
+	console.log('deliucious xhr data', data)
+})
+
+```
 
 
 your event nor interval does nt start until you call stream.sub(function), after you do, each incomming item will call your function
 
 
-stream.map(fn) returns new stream connected to current stream 
+stream.map(fn) returns new stream connected to current stream, each value is mdified by fn(value)
+
+
+stream.flatMap(fn) subscribes to each received item and returns fn(value) each time one of inner streams returns value
 
 
 stream.zip(otherStream, fn) calls fn(x,y) with items from each stream, if one stream has no items, execution waits for its arrival, effectively returning stream composed of both streams
--- sob.js
+++ sob.js
@@ -1,182 +1,177 @@
 var Sob = (function(){
-  var Sob = function(run, dispose){
-    this.run = run;
-    this.dispose = dispose;
-    this._count = 0;
-    this._subs = [];
-  };
-  
-  Sob.prototype.onNext = function(value){
-    this._count+=1;
-    this._subs.forEach(function(sub){
-      sub(value);
-    });
-  };
-  
-  Sob.prototype.sub = function(fn){
-    if(!this.active)
-      this.run();
-    this._subs.push(fn);
-  };
-  
-  Sob.prototype.unsub = function(fn){
-    this._subs.splice(this._subs.indexOf(fn), 1);
-  };
-  
-  Sob.prototype.hasSubs = function(fn){
-    return this._subs.length > 0;
-  };
-  
-  Sob.prototype.map = function(fn){
-    var that = this;
-    var sob = new Sob(
-      function(){
-        sob.active = true;
-        that.sub(cb);
-      },
-      function(){
-        sob.active = false;
-        that.unsub(cb);
-      }
-    );
-    var cb = function(data){
-      sob.onNext(fn(data));
-    };
-
-    return sob;
-  };
-  
-  Sob.prototype.flatMap = function(fn){
-    var that = this;
-    var sob = new Sob(
-      function(){
-        sob.active = true;
-        that.sub(cb);
-      },
-      function(){
-        sob.active = false;
-        that.unsub(cb);
-      }
-    );
-    var cb = function(obs){
-      obs.sub(function(data){
-        sob.onNext(fn(data));
-      });
-    };
-
-    return sob;
-  };
-  
-  Sob.prototype.zip = function(other, fn){
-    var that = this;
-    var sob = new Sob(
-      function(){
-        sob.active = true;
-        that.sub(cb1);
-        other.sub(cb2);
-      },
-      function(){
-        sob.active = false;
-        that.unsub(cb1);
-        other.unsub(cb2);
-      }
-    );
-    
-    var thatVals = [];
-    var otherVals = [];
-    
-    var onNext = function(){
-      if(thatVals.length && otherVals.length)
-        sob.onNext( fn(thatVals.shift(), otherVals.shift()) );
-    };
-    
-    var cb1 = function(data){
-      thatVals.push(data);
-      onNext();
-    };
-    var cb2 = function(data){
-      otherVals.push(data);
-      onNext();
-    };
-
-    return sob;
-  };
-  
-  Sob.prototype.first = function(){
-    var that = this;
-    var sob = new Sob(
-      function(){
-        sob.active = true;
-        that.sub(cb);
-      },
-      function(){
-        sob.active = false;
-        that.unsub(cb);
-      }
-    );
-    var cb = function(data){
-      sob.onNext(data);
-      sob.dispose();
-    };
-
-    return sob;
-  };
-  
-  
-  Sob.fromDOMEvent = function(obj, ev){
-    var sob = new Sob(
-      function(){
-        sob.active = true;
-        obj.addEventListener(ev, cb);
-      },
-      function(){
-        sob.active = false;
-        obj.removeEventListener(ev, cb);
-      }
-    );
-    var cb = function(){
-      sob.onNext(Array.prototype.slice.call(arguments));
-    };
-
-    return sob;
-  };
-  
-  Sob.fromInterval = function(time){
-    var sob = new Sob(
-      function(){
-        sob.active = true;
-        itv = setInterval(function(){
-          sob.onNext(sob._count);
-        }, time);
-      },
-      function(){
-        sob.active = false;
-        clearInterval(itv);
-      }
-    );
-    var itv;
-
-    return sob;
-  };
-  
-  Sob.fromTimeout = function(time){
-    var sob = new Sob(
-      function(){
-        sob.active = true;
-        itv = setTimeout(function(){
-          sob.onNext(sob._count);
-          sob.dispose();
-        }, time);
-      },
-      function(){
-        sob.active = false;
-        clearTimeout(itv);
-      }
-    );
-    var itv;
-
-    return sob;
-  };
-  
-  return Sob;
+	var Sob = function(run, dispose){
+		
+		this.run = function(){
+			this.active = true;
+			run(this.onNext);
+		}.bind(this);
+		
+		this.dispose = function(){
+			this.active = false;
+			dispose();
+		}.bind(this);
+		
+		this._count = 0;
+		this._subs = [];
+	};
+	
+	Sob.prototype.onNext = function(value){
+		this._count+=1;
+		this._subs.forEach(function(sub){
+			sub(value);
+		});
+	};
+	
+	Sob.prototype.sub = function(fn){
+		if(!this.active)
+		this.run();
+		this._subs.push(fn);
+	};
+	
+	Sob.prototype.unsub = function(fn){
+		this._subs.splice(this._subs.indexOf(fn), 1);
+	};
+	
+	Sob.prototype.hasSubs = function(fn){
+		return this._subs.length > 0;
+	};
+	
+	Sob.prototype.map = function(fn){
+		var that = this;
+		var sob = new Sob(
+			function(){
+				that.sub(cb);
+			},
+			function(){
+				that.unsub(cb);
+			}
+		);
+		var cb = function(data){
+			sob.onNext(fn(data));
+		};
+		
+		return sob;
+	};
+	
+	Sob.prototype.flatMap = function(fn){
+		var that = this;
+		var sob = new Sob(
+			function(){
+				that.sub(cb);
+			},
+			function(){
+				that.unsub(cb);
+			}
+		);
+		var cb = function(obs){
+			obs.sub(function(data){
+				sob.onNext(fn(data));
+			});
+		};
+		
+		return sob;
+	};
+	
+	Sob.prototype.zip = function(other, fn){
+		var that = this;
+		var sob = new Sob(
+			function(){
+				that.sub(cb1);
+				other.sub(cb2);
+			},
+			function(){
+				that.unsub(cb1);
+				other.unsub(cb2);
+			}
+		);
+		
+		var thatVals = [];
+		var otherVals = [];
+		
+		var onNext = function(){
+			if(thatVals.length && otherVals.length)
+			sob.onNext( fn(thatVals.shift(), otherVals.shift()) );
+		};
+		
+		var cb1 = function(data){
+			thatVals.push(data);
+			onNext();
+		};
+		var cb2 = function(data){
+			otherVals.push(data);
+			onNext();
+		};
+		
+		return sob;
+	};
+	
+	Sob.prototype.first = function(){
+		var that = this;
+		var sob = new Sob(
+			function(){
+				that.sub(cb);
+			},
+			function(){
+				that.unsub(cb);
+			}
+		);
+		var cb = function(data){
+			sob.onNext(data);
+			sob.dispose();
+		};
+		
+		return sob;
+	};
+	
+	
+	Sob.fromDOMEvent = function(obj, ev){
+		var sob = new Sob(
+			function(){
+				obj.addEventListener(ev, cb);
+			},
+			function(){
+				obj.removeEventListener(ev, cb);
+			}
+		);
+		var cb = function(){
+			sob.onNext(Array.prototype.slice.call(arguments));
+		};
+		
+		return sob;
+	};
+	
+	Sob.fromInterval = function(time){
+		var sob = new Sob(
+			function(){
+				itv = setInterval(function(){
+					sob.onNext(sob._count);
+				}, time);
+			},
+			function(){
+				clearInterval(itv);
+			}
+		);
+		var itv;
+		
+		return sob;
+	};
+	
+	Sob.fromTimeout = function(time){
+		var sob = new Sob(
+			function(){
+				itv = setTimeout(function(){
+					sob.onNext(sob._count);
+					sob.dispose();
+				}, time);
+			},
+			function(){
+				clearTimeout(itv);
+			}
+		);
+		var itv;
+		
+		return sob;
+	};
+	
+	return Sob;
 })();
