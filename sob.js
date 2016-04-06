2ed65404ba4ccbb42258496f5bb4ffe917cd0cfa
pm <=>
2016-04-06 02:46:02 +0200

    flatmap, fromTimeout, som constructor arguemnts
    * Sob(run, dispose) nwo takes two arguments

--- sob.js
+++ sob.js
@@ -1,11 +1,12 @@
 var Sob = (function(){
-  var Sob = function(){
+  var Sob = function(run, dispose){
+    this.run = run;
+    this.dispose = dispose;
     this._count = 0;
     this._subs = [];
   };
   
   Sob.prototype.onNext = function(value){
-    //console.log(value);
     this._count+=1;
     this._subs.forEach(function(sub){
       sub(value);
@@ -19,7 +20,7 @@ var Sob = (function(){
   };
   
   Sob.prototype.unsub = function(fn){
-    this._subs.splice(this._subs.indexOf(fn), 1)
+    this._subs.splice(this._subs.indexOf(fn), 1);
   };
   
   Sob.prototype.hasSubs = function(fn){
@@ -28,24 +29,58 @@ var Sob = (function(){
   
   Sob.prototype.map = function(fn){
     var that = this;
-    var sob = new Sob();
+    var sob = new Sob(
+      function(){
+        sob.active = true;
+        that.sub(cb);
+      },
+      function(){
+        sob.active = false;
+        that.unsub(cb);
+      }
+    );
     var cb = function(data){
       sob.onNext(fn(data));
     };
-    sob.run = function(){
-      sob.active = true;
-      that.sub(cb);
+
+    return sob;
+  };
+  
+  Sob.prototype.flatMap = function(fn){
+    var that = this;
+    var sob = new Sob(
+      function(){
+        sob.active = true;
+        that.sub(cb);
+      },
+      function(){
+        sob.active = false;
+        that.unsub(cb);
+      }
+    );
+    var cb = function(obs){
+      obs.sub(function(data){
+        sob.onNext(fn(data));
+      });
     };
-    sob.dispose = function(){
-      sob.active = false;
-      that.unsub(cb);
-    }
+
     return sob;
   };
   
   Sob.prototype.zip = function(other, fn){
     var that = this;
-    var sob = new Sob();
+    var sob = new Sob(
+      function(){
+        sob.active = true;
+        that.sub(cb1);
+        other.sub(cb2);
+      },
+      function(){
+        sob.active = false;
+        that.unsub(cb1);
+        other.unsub(cb2);
+      }
+    );
     
     var thatVals = [];
     var otherVals = [];
@@ -53,78 +88,93 @@ var Sob = (function(){
     var onNext = function(){
       if(thatVals.length && otherVals.length)
         sob.onNext( fn(thatVals.shift(), otherVals.shift()) );
-    }
+    };
     
     var cb1 = function(data){
-      thatVals.push(data)
-      onNext()
-    };
-    var cb2 = function(data){
-      otherVals.push(data)
+      thatVals.push(data);
       onNext();
     };
-    sob.run = function(){
-      sob.active = true;
-      that.sub(cb1);
-      other.sub(cb2);
+    var cb2 = function(data){
+      otherVals.push(data);
+      onNext();
     };
-    sob.dispose = function(){
-      sob.active = false;
-      that.unsub(cb1)
-      other.unsub(cb2)
-    }
+
     return sob;
   };
   
   Sob.prototype.first = function(){
     var that = this;
-    var sob = new Sob();
+    var sob = new Sob(
+      function(){
+        sob.active = true;
+        that.sub(cb);
+      },
+      function(){
+        sob.active = false;
+        that.unsub(cb);
+      }
+    );
     var cb = function(data){
-      sob.onNext(data)
+      sob.onNext(data);
       sob.dispose();
     };
-    sob.run = function(){
-      sob.active = true;
-      that.sub(cb);
-    };
-    sob.dispose = function(){
-      sob.active = false;
-      that.unsub(cb)
-    }
+
     return sob;
   };
   
   
   Sob.fromDOMEvent = function(obj, ev){
-    var sob = new Sob();
+    var sob = new Sob(
+      function(){
+        sob.active = true;
+        obj.addEventListener(ev, cb);
+      },
+      function(){
+        sob.active = false;
+        obj.removeEventListener(ev, cb);
+      }
+    );
     var cb = function(){
       sob.onNext(Array.prototype.slice.call(arguments));
     };
-    sob.run = function(){
-      sob.active = true;
-      obj.addEventListener(ev, cb);
-    }
-    sob.dispose = function(){
-      sob.active = false;
-      obj.removeEventListener(ev, cb);
-    };
+
     return sob;
   };
   
   Sob.fromInterval = function(time){
-    var sob = new Sob();
+    var sob = new Sob(
+      function(){
+        sob.active = true;
+        itv = setInterval(function(){
+          sob.onNext(sob._count);
+        }, time);
+      },
+      function(){
+        sob.active = false;
+        clearInterval(itv);
+      }
+    );
     var itv;
-    sob.run = function(){
-      sob.active = true;
-      itv = setInterval(function(){
-        sob.onNext(sob._count);
-      }, time);
 
-    }
-    sob.dispose = function(){
-      sob.active = false;
-      clearInterval(itv);
-    };
+    return sob;
+  };
+  
+  Sob.fromTimeout = function(time){
+    var sob = new Sob(
+      function(){
+        sob.active = true;
+        itv = setTimeout(function(){
+          sob.onNext(sob._count);
+          sob.dispose();
+        }, time);
+      },
+      function(){
+        sob.active = false;
+        clearTimeout(itv);
+      }
+    );
+    var itv;
+
     return sob;
   };
   
